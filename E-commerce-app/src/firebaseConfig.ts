598c11fb4ca61c953f4731c14939734ca2e283c4
Ferdinand Niyongire <ferdinandniyongira@gmail.com>
2025-07-10 14:56:37 -0500

    Updates

--- E-commerce-app/src/firebaseConfig.ts
+++ E-commerce-app/src/firebaseConfig.ts
@@ -9,9 +9,11 @@ const firebaseConfig = {
   storageBucket: "e-commerce-firebase-app-3319a.firebasestorage.app",
   messagingSenderId: "154779788627",
   appId: "1:154779788627:web:ced09eb8a26456945c6da6"
+  
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+console.log(import.meta.env.VITE_FIREBASE_API_KEY)
 
